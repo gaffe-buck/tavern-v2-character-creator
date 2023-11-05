15c0286bb33d05db865ec9217c3e2ad46d9f6f01
gaffe-buck <gaffe@noreply>
2023-11-05 14:04:38 -0500

    fix link to source

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function Footer() {
-    return <p className="text-xs m-4">No warranty express or implied, I will not be responsible for the horrors you unleash on the world. Source code available <a className="text-interactive" href="">here</a>.</p>
+    return <p className="text-xs m-4">No warranty express or implied, I will not be responsible for the horrors you unleash on the world. Source code available <a className="text-interactive" href="https://github.com/gaffe-buck/tavern-v2-character-creator">here</a>.</p>
 }
